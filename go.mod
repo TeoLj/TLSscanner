fa79e579113b7214b8e05e15f25484068e570168
Teodora Ljubevska <teodora.ljubevska@tum.de>
2024-02-12 17:19:58 +0100

    read CSV works

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/TeoLj/TLSscanner_FP.git
 
 go 1.18
+
+require github.com/dchest/validator v0.0.0-20191217151620-8e45250f2371 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"net/url"
+	"os"
 	"strings"
 	"time"
+	
+
+	//"github.com/dchest/validator"
 )
 
 func main() {
@@ -12,18 +18,24 @@ func main() {
 	fmt.Println("\033[1;35mStart:", start.Format("2006-01-02 15:04:05"), "\033[0m")
 
 	opts := ParseFlags()
+	
 
 	flag.Parse() // execute the command-line parsing
+	var domains []string
 
-	if opts.DomainsList == "" {
-		println("Please specify a list of domains")
-		return
-	}
-
-	domains := strings.Split(opts.DomainsList, ",")
-	for i, domain := range domains {
-		domains[i] = strings.TrimSpace(domain) // Trim spaces from each domain
-	}
+	
+    if opts.CSVFilePath != "" {
+        var err error
+        domains, err = readCSV(opts.CSVFilePath, opts.EntriesToScan)
+		
+        if err != nil {
+            fmt.Println("Error reading CSV file:", err)
+            return
+        }
+    } else if opts.DomainsList != "" {
+        domains = parseDomainsList(opts.DomainsList)
+    }
+	
 
 	scanner := NewScanner(domains, opts.Concurrency, opts.Timeout, opts.EntriesToScan, opts.CSVFilePath)
 	scanner.StartScanner()
@@ -31,4 +43,57 @@ func main() {
 	end := time.Now()
 	fmt.Println("\033[1;35mEnd:", end.Format("15:04:05"), "Duration:", end.Sub(start), "\033[0m")
 
+}
+
+
+
+
+func readCSV(filePath string, entriesToScan int) ([]string, error) {
+	
+    file, err := os.Open(filePath)
+    if err != nil {
+        return nil, err
+    }
+    defer file.Close()
+
+    var domains []string
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        if entriesToScan > 0 && len(domains) >= entriesToScan {
+            break
+        }
+        line := scanner.Text()
+        record := strings.Split(line, ",")
+        for _, field := range record {
+            domain, err := extractDomain(field)
+            if err == nil && domain != "" {
+                domains = append(domains, domain)
+            }
+        }
+    }
+    if err := scanner.Err(); err != nil {
+        return nil, err
+    }
+
+    return domains, nil
+}
+
+func extractDomain(field string) (string, error) {
+	
+    field = strings.TrimSpace(field)
+    if strings.HasPrefix(field, "http://") || strings.HasPrefix(field, "https://") {
+        u, err := url.Parse(field)
+        if err != nil {
+            return "", err
+        }
+		
+        return u.Hostname(), nil
+    } else if strings.HasPrefix(field, "www.") || strings.HasSuffix(field, ".com") {
+        return field, nil
+    }
+    return "", fmt.Errorf("invalid domain format")
+}
+
+func parseDomainsList(domainsList string) []string {
+    return strings.Split(domainsList, ",")
 }
